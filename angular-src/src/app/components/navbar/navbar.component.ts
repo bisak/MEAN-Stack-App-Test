71e2cbb8f30df6761fb83541d4db65743d55cbba
Biser Atanasov <biser.p.atanasov@gmail.com>
2017-02-22 11:24:40 +0200

    Initial commit

---
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router'
+import {FlashMessagesService} from 'angular2-flash-messages'
+import {AuthService} from '../../services/auth.service'
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private flashMessage: FlashMessagesService) {
+  }
+
+  ngOnInit() {
+  }
+
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessage.show('Logout Success!', {cssClass: 'alert-success', timeout: 1000});
+    this.router.navigate(['login'])
+    return false;
+  }
+
+}
